9232fa4b77a6f1303aa014fcace4708ddd2a9c53
George Victor <george.vbm@hotmail.com>
2024-08-18 20:42:49 -0300

    Deixando menu funcional

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { bootstrapList } from '@ng-icons/bootstrap-icons';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIconComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.scss',
+  viewProviders: [provideIcons({ bootstrapList })],
 })
 export class HeaderComponent {
+  opened = false;
 
+  toggleMenu() {
+    console.log('Alterou o menu');
+
+    this.opened = !this.opened;
+  }
 }
