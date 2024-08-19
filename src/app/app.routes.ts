d9f6b86b3fb710d505f0bbfa3bdc2e2414f5999b
George Victor <george.vbm@hotmail.com>
2024-08-19 20:51:01 -0300

    chore: insere eslint e prettier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,30 +4,28 @@ export const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'home',
     loadComponent: () =>
-      import('./pages/home/home.component').then((m) => m.HomeComponent),
+      import('./pages/home/home.component').then(m => m.HomeComponent),
   },
   {
     path: 'skills',
     loadComponent: () =>
-      import('./pages/skills/skills.component').then((m) => m.SkillsComponent),
+      import('./pages/skills/skills.component').then(m => m.SkillsComponent),
   },
   {
     path: 'projects',
     loadComponent: () =>
       import('./pages/projects/projects.component').then(
-        (m) => m.ProjectsComponent
+        m => m.ProjectsComponent
       ),
   },
   {
     path: 'contact',
     loadComponent: () =>
-      import('./pages/contact/contact.component').then(
-        (m) => m.ContactComponent
-      ),
+      import('./pages/contact/contact.component').then(m => m.ContactComponent),
   },
 ];
