f8fa84e0be21e1ee2d53d690fc202910488ac0c2
George Victor <george.vbm@hotmail.com>
2024-08-20 01:15:11 -0300

    style(menu): altera animacao do botao de abrir o menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { bootstrapList } from '@ng-icons/bootstrap-icons';
+import { bootstrapList, bootstrapX } from '@ng-icons/bootstrap-icons';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 
 @Component({
@@ -9,7 +9,7 @@ import { NgIconComponent, provideIcons } from '@ng-icons/core';
   imports: [CommonModule, NgIconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  viewProviders: [provideIcons({ bootstrapList })],
+  viewProviders: [provideIcons({ bootstrapList, bootstrapX })],
 })
 export class HeaderComponent {
   opened = false;
