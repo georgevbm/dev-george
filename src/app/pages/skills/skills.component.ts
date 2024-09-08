b0bbb3c3c138f5838958a2a160decfcfb9f07e56
George Victor <george.vbm@hotmail.com>
2024-09-08 15:56:56 -0300

    [✨] feat: cria estrutura da pagina de habilidades

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -1,10 +1,100 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { SkillBullet } from './types/bullet-skills.interface';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { bootstrapPeopleFill } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, NgIconComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
+  viewProviders: [
+    provideIcons({
+      bootstrapPeopleFill,
+    }),
+  ],
 })
-export class SkillsComponent {}
+export class SkillsComponent {
+  skillBullets: SkillBullet[] = [
+    // Primeira linha
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    // Segunda linha
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    // Terceiera linha
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+    {
+      url: 'image-skills/angular-logo.png',
+      alt: 'Logotipo do Angular',
+      backgroundColor: '#FFFFFF',
+    },
+  ];
+}
---
+++ src/app/pages/skills/types/bullet-skills.interface.ts
@@ -0,0 +1,5 @@
+export interface SkillBullet {
+  url: string;
+  alt: string;
+  backgroundColor: string;
+}
