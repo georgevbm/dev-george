3e25555ec626253c16c3ae9f468b46103e3a943b
George Victor <george.vbm@hotmail.com>
2024-09-29 04:22:44 -0300

    [✅] test: ajusta erro por usas scss

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,2 +1,12 @@
 import 'jest-axe/extend-expect';
 import 'jest-preset-angular/setup-jest';
+
+const originalConsoleError = console.error;
+console.error = function (...data) {
+  if (
+    typeof data[0]?.toString === 'function' &&
+    data[0].toString().includes('Error: Could not parse CSS stylesheet')
+  )
+    return;
+  originalConsoleError(...data);
+};
